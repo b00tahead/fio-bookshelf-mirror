744b35ff57f9128023645ebcb487e4a3a88c9477
Scott <smay44@gmail.com>
2024-03-07 20:41:52 -0500

    Port over code from pantheon repo

---
+++ web/themes/custom/bookshelf/js/bookshelf.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * bookshelf behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.bookshelf = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
